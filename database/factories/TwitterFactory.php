ad4a9ad144d178dde145a3cb330c061be4e340de
zeinab-a98 <you@example.comzeinababbasi98@gmail.com>
2019-10-10 09:25:42 +0330

    init

---
+++ database/factories/TwitterFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\twitter;
+use Faker\Generator as Faker;
+
+$factory->define(twitter::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/twitter', function () {
+    return view('twitter');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
