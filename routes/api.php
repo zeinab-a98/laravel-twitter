e796f2137550e61d2ab85f8cb70bc8c06708b54b
zeinab-a98 <you@example.comzeinababbasi98@gmail.com>
2019-10-11 21:28:23 +0330

    commit kernel.php cors and api

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+    //return $request->user();
     
-});
+//});
+//Route::apiResource('category', 'CategoryController')->middleware('cors');
+//Route::apiResource('product', 'ProductController')->middleware('cors');
 
-Route::get('/sms', 'SmsController@index');
-
-Route::post('/sms', 'SmsController@post');
+Route::get('/sms', 'SmsController@index')->middleware('cors');
+Route::post('/sms', 'SmsController@post')->middleware('cors');
